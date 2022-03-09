757eac91bca1f3113d11cb00d413af1995805352
Wilk, Nicholas <nicholas.wilk@mines.sdsmt.edu>
2022-03-09 11:06:45 -0700

    thpe2 -[ci skip] - finished sqrt function

--- thpe2/thpe2.cpp
+++ thpe2/thpe2.cpp
@@ -1,11 +1,37 @@
 #include "thpe2.h"
 
-double  mySqrt( double stNum)
+double  mySqrt( double &stNum)
 {
-    double check = stNum;
-    if( check <= 0 )
+    int i = 0;
+    double check;
+    double low = 0;
+    double mid;
+    double max = stNum;
+    if( max <= 0 )
     {
         return 0;
     }
+    while( i <= 5000)
+    {
+        mid = (max + low) / 2;
+        check = pow( mid, 2);
+        if( (fabs(check - stNum)) < ERRORVALUE)
+            {
+                return mid;
+            }
+            if( (fabs(check - stNum)) > ERRORVALUE)
+            {
+                if( check > stNum)
+                {
+                    max = mid;
+                    i++;
+                }
+                if( check < stNum)
+                {
+                    low = mid;
+                    i++;
+                }
+            }
+    }
+    return 0;
 }
-
--- thpe2/thpe2Tests.cpp
+++ thpe2/thpe2Tests.cpp
@@ -9,3 +9,47 @@ TEST_CASE( "mySqrt - testing 0, should return 0")
     ans = mySqrt( stNum);
     REQUIRE( ans == 0);
 }
+
+
+TEST_CASE( "mySqrt - testing 4, should return 2")
+{
+    double stNum = 4;
+    double ans;
+    ans = mySqrt( stNum);
+    REQUIRE( ans == Approx(2));
+}
+
+
+TEST_CASE( "mySqrt - testing 9, should return 3")
+{
+    double stNum = 9;
+    double ans;
+    ans = mySqrt( stNum);
+    REQUIRE( ans == Approx(3));
+}
+
+TEST_CASE( "mySqrt - testing 16, should return 4")
+{
+    double stNum = 16;
+    double ans;
+    ans = mySqrt( stNum);
+    REQUIRE( ans == Approx(4));
+}
+
+TEST_CASE( "mySqrt - testing 435, should return 20.856654")
+{
+    double stNum = 435;
+    double ans;
+    ans = mySqrt( stNum);
+    REQUIRE( ans == Approx(20.8566536146));
+}
+
+TEST_CASE( "mySqrt - testing 22.8, should return 4.77493455453")
+{
+    double stNum = 22.8;
+    double ans;
+    ans = mySqrt( stNum);
+    REQUIRE( ans == Approx( 4.77493455453));
+}
+
+
